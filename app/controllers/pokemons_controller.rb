ce0fc8ffb4643b71f2426f51c0265a6b40df9cb0
rahmad2000 <139571072+rahmad2000@users.noreply.github.com>
2024-04-16 05:42:15 -0500

    Finished Search System

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,18 +1,19 @@
 class PokemonsController < ApplicationController
   def index
+    @search_performed = params[:search].present?
     base_query = Pokemon.joins(:types, :abilities)
-                        .where.not(types: { id: nil }, abilities: { id: nil })  # Ensure Pokemon has types and abilities
-                        .where.not(url: [nil, ''])  # Ensure image url is present
+                        .where.not(types: { id: nil }, abilities: { id: nil })
+                        .where.not(url: [nil, ''])
                         .distinct
 
-    if params[:search].present?
-      @pokemons = base_query.where('name LIKE ?', "%#{params[:search]}%")
-    else
-      @pokemons = base_query
+    if @search_performed
+      base_query = base_query.where('pokemons.name LIKE ?', "%#{params[:search]}%")
     end
 
-    @pokemons = @pokemons.page(params[:page])  # Apply pagination if necessary
-    @pokemons = @pokemons.presence || []  # Ensures @pokemons is always an array
+    @pokemons = base_query.page(params[:page])
+
+    # Using @pokemons.empty? will check if the query returned results
+    @no_results = @pokemons.empty? && @search_performed
   end
 
   def show
